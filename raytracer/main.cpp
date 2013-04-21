04a06c4c9d68f13cdce3e4bb2c1cd2d096a7c977
ambling <ambling07@gmail.com>
2013-04-21 22:35:52 +0800

    Initial Commit

---
+++ raytracer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  raytracer
+//
+//  Created by ambling on 13-4-21.
+//  Copyright (c) 2013年 ambling. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
